09a90e4bf29442c7fb796f297c3d7acb0825fa90
Rodrigo Augusto <rodrigoaugusto96@outlook.com>
2021-05-19 19:09:01 -0300

    Sum of angles of a triangle exercise

---
+++ Fundamentos do Desenvolvimento Web/bloco_4/exercises/introducao-a-javascript-e-logica-de-programacao_1/triangle.js
@@ -0,0 +1,13 @@
+let a = 50;
+let b = 50;
+let c = 80;
+
+if (a + b + c === 180) {
+  if (true) {
+    console.log(true);
+  } else {
+    console.log(false);
+  }
+} else {
+  console.log("Valor de ângulo invalido");
+}
