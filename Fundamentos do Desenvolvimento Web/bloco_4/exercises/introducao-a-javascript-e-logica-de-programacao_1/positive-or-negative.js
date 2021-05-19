b9963c3e25e5ed0f37284cdea59bd3c4cbd76406
Rodrigo Augusto <rodrigoaugusto96@outlook.com>
2021-05-19 18:57:25 -0300

    Create positive or negative exercise

---
+++ Fundamentos do Desenvolvimento Web/bloco_4/exercises/introducao-a-javascript-e-logica-de-programacao_1/positive-or-negative.js
@@ -0,0 +1,9 @@
+let a = 5;
+
+if (a > 0) {
+  console.log('O número ' + a + ' é positivo.');
+} else if (a === 0) {
+  console.log('O número ' + a + ' é zero.');
+} else {
+  console.log('O número ' + a + ' é negativo.');
+}
