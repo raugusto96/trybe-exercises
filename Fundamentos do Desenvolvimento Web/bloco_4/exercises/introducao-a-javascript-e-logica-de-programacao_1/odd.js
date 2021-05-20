b3eb76b87d279eaf5e5d0bf1683a50f2755855df
Rodrigo Augusto <rodrigoaugusto96@outlook.com>
2021-05-19 21:19:21 -0300

    Odd exercise

---
+++ Fundamentos do Desenvolvimento Web/bloco_4/exercises/introducao-a-javascript-e-logica-de-programacao_1/odd.js
@@ -0,0 +1,11 @@
+let a = 4;
+let b = 10;
+let c = 16;
+let isTrue = true;
+let isFalse = false;
+
+if (a % 2 == 1 || b % 2 == 1 || c % 2 == 1) {
+  console.log(isTrue);
+} else {
+  console.log(isFalse);
+}
