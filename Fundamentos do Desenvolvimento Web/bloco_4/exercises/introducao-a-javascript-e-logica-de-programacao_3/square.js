e985f45f6162f787f3b60dddcaece1d33b5cfc1e
Rodrigo Augusto <rodrigoaugusto96@outlook.com>
2021-05-21 18:50:12 -0300

    Fix some stuffs

--- Fundamentos do Desenvolvimento Web/bloco_4/exercises/introducao-a-javascript-e-logica-de-programacao_3/square.js
+++ Fundamentos do Desenvolvimento Web/bloco_4/exercises/introducao-a-javascript-e-logica-de-programacao_3/square.js
@@ -3,11 +3,9 @@ let asterisco = '*';
 let blankRow = '';
 
 for (let row = 0; row < n; row += 1) {
-  // percorro as linhas
   for (let column = 0; column < n; column += 1) {
-    // percorro as colunas
-    blankRow += asterisco; // adiciona um asterisco a cada coluna da minha linha
+    blankRow += asterisco;
   }
-  console.log(blankRow); // imprimo a linha
-  blankRow = ''; // zero a linha antes de começar o processo de novo.
+  console.log(blankRow);
+  blankRow = '';
 }
