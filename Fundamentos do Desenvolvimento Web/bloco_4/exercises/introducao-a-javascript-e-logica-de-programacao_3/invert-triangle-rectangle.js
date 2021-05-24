9efcedc288d3ac681b3c7e6416df401503e027ca
Rodrigo Augusto <rodrigoaugusto96@outlook.com>
2021-05-24 12:35:29 -0300

    Fix some stuffs

--- Fundamentos do Desenvolvimento Web/bloco_4/exercises/introducao-a-javascript-e-logica-de-programacao_3/invert-triangle-rectangle.js
+++ Fundamentos do Desenvolvimento Web/bloco_4/exercises/introducao-a-javascript-e-logica-de-programacao_3/invert-triangle-rectangle.js
@@ -7,9 +7,7 @@ for (let row = 0; row < n; row += 1) {
   count = row;
   blankRow = ' ';
   for (let column = 0; column <= n; column += 1) {
-    if (column == n) {
-      blankRow += asterisk;
-    } else if (column == n - count) {
+    if (column == n - count) {
       blankRow += asterisk;
       count -= 1;
     } else {
