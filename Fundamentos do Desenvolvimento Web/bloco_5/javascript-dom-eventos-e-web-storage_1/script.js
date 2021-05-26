e60bf2a23160ae9977943ee56b437db2685aa5da
Rodrigo Augusto <rodrigoaugusto96@outlook.com>
2021-05-26 16:33:05 -0300

    Part 3 - DOM Selectors

---
+++ Fundamentos do Desenvolvimento Web/bloco_5/javascript-dom-eventos-e-web-storage_1/script.js
@@ -0,0 +1,28 @@
+document.getElementsByTagName('body')[0].style.backgroundColor = 'gray';
+
+let header = document.getElementsByTagName('header');
+header[0].style.backgroundColor = 'green';
+header[0].style.padding = '1.1em';
+header[0].style.textAlign = 'center';
+header[0].style.margin = '0';
+header[0].style.fontSize = '1em';
+header[0].style.color = 'white';
+
+let footer = document.getElementById('footer-container');
+footer.style.backgroundColor = 'blue';
+footer.style.color = 'white';
+footer.style.fontSize = '1em';
+footer.style.textAlign = 'center';
+footer.style.position = 'absolute bottom';
+
+let section = document.getElementsByClassName('emergency-tasks');
+section[0].style.backgroundColor = 'pink';
+
+let section2 = document.getElementsByClassName('no-emergency-tasks');
+section2[0].style.backgroundColor = 'olive';
+
+let h3 = document.getElementsByTagName('h3');
+h3[0].style.backgroundColor = 'darkviolet';
+h3[1].style.backgroundColor = 'darkviolet';
+h3[2].style.backgroundColor = 'black';
+h3[3].style.backgroundColor = 'black';
