2497a95578c91391b42b24c797499b3bf90d8a53
Rodrigo Augusto <rodrigoaugusto96@outlook.com>
2021-05-28 19:54:05 -0300

    Exercise 1

--- Fundamentos do Desenvolvimento Web/bloco_5/javascript-dom-eventos-e-web-storage_3/exercises/script.js
+++ Fundamentos do Desenvolvimento Web/bloco_5/javascript-dom-eventos-e-web-storage_3/exercises/script.js
@@ -13,4 +13,27 @@ function createDaysOfTheWeek() {
 
 createDaysOfTheWeek();
 
+
 // Escreva seu código abaixo.
+
+// Exercício 1
+function createNumberDays() {
+  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+  const monthDaysList = document.querySelector('#days');
+  for (let index = 0; index < dezDaysList.length; index += 1) {
+    const days = dezDaysList[index];
+    const dayListItem = document.createElement('li');
+    dayListItem.className = 'day';
+    dayListItem.innerHTML = days;
+
+    if (days === 24 || days === 25 || days === 31) {
+      dayListItem.className = 'day holiday';
+    }
+    if (days === 4 || days === 11 || days === 18 || days === 25) {
+      dayListItem.className = 'day friday'
+    }
+    monthDaysList.appendChild(dayListItem);
+  }
+}
+
+createNumberDays();
