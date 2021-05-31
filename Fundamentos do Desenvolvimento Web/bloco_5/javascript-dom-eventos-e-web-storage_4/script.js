2eda91fa0a1af4f516c988b3451e2c77d8c6ba54
Rodrigo Augusto <rodrigoaugusto96@outlook.com>
2021-05-31 19:29:08 -0300

    Second color

--- Fundamentos do Desenvolvimento Web/bloco_5/javascript-dom-eventos-e-web-storage_4/script.js
+++ Fundamentos do Desenvolvimento Web/bloco_5/javascript-dom-eventos-e-web-storage_4/script.js
@@ -3,6 +3,10 @@ const backgroundColor = document.querySelector('.background-container');
 const color1 = document.createElement('div');
 color1.style.backgroundColor = 'green';
 backgroundColor.appendChild(color1);
+
+const color2 = document.createElement('div');
+color2.style.backgroundColor = 'lightblue';
+backgroundColor.appendChild(color2);
 // Cor da fonte
 
 // Tamanho da Fonte
