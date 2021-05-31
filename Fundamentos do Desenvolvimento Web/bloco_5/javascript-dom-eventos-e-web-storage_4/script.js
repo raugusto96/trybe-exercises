17281865d6c0f6a6e88967baf0780cb04c34134c
Rodrigo Augusto <rodrigoaugusto96@outlook.com>
2021-05-31 19:28:04 -0300

    First color

--- Fundamentos do Desenvolvimento Web/bloco_5/javascript-dom-eventos-e-web-storage_4/script.js
+++ Fundamentos do Desenvolvimento Web/bloco_5/javascript-dom-eventos-e-web-storage_4/script.js
@@ -1,11 +1,12 @@
-window.onload = function () {
-  // Cor de fundo
+// Cor do Fundo
+const backgroundColor = document.querySelector('.background-container');
+const color1 = document.createElement('div');
+color1.style.backgroundColor = 'green';
+backgroundColor.appendChild(color1);
+// Cor da fonte
 
-  // Cor da fonte
+// Tamanho da Fonte
 
-  // Tamanho da Fonte
+// Estilo da Fonte
 
-  // Estilo da Fonte
-
-  // Espessura da linha
-}
+// Espessura da linha
