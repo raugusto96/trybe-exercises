006398609c6fcdaaf1c99f53b3ea094a2f58976b
Rodrigo Augusto <rodrigoaugusto96@outlook.com>
2021-05-31 19:30:13 -0300

    Third color

--- Fundamentos do Desenvolvimento Web/bloco_5/javascript-dom-eventos-e-web-storage_4/script.js
+++ Fundamentos do Desenvolvimento Web/bloco_5/javascript-dom-eventos-e-web-storage_4/script.js
@@ -7,6 +7,10 @@ backgroundColor.appendChild(color1);
 const color2 = document.createElement('div');
 color2.style.backgroundColor = 'lightblue';
 backgroundColor.appendChild(color2);
+
+const color3 = document.createElement('div');
+color3.style.backgroundColor = 'rgb(75, 75, 75)';
+backgroundColor.appendChild(color3);
 // Cor da fonte
 
 // Tamanho da Fonte
