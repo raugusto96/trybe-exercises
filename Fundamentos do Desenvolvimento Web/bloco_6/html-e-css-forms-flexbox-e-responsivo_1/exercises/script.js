a7d963970200d8cd5b42b5a925f258dd331e404f
Rodrigo Augusto <rodrigoaugusto96@outlook.com>
2021-06-09 20:36:46 -0300

    Finish exercise 2

--- Fundamentos do Desenvolvimento Web/bloco_6/html-e-css-forms-flexbox-e-responsivo_1/exercises/script.js
+++ Fundamentos do Desenvolvimento Web/bloco_6/html-e-css-forms-flexbox-e-responsivo_1/exercises/script.js
@@ -35,3 +35,28 @@ for (let key in brazilStates) {
   stateOption.innerHTML = brazilStates[key];
   statesDropdown.appendChild(stateOption);
 }
+
+// const inputDate = document.getElementById('date');
+
+// inputDate.addEventListener('input', () => {
+//   const startDate = document.getElementById('date').value;
+//   let day = Number(startDate[0] + startDate[1]); 
+//   let month = Number(startDate[3] + startDate[4]);
+//   let year = Number(startDate[6] + startDate[7] + startDate[8] + startDate[9]);
+    
+//   if (day > 0) {
+//     console.log('Funciona');
+//   } 
+//   else {
+//     alert('O dia é inválido');
+//   }
+  // if (month > 0 && month <= 12) {
+  //   alert('O mês é inválido');
+  // }
+  // if (year > 0) {
+  //   alert('O ano é inválido');
+  // }
+// });
+
+
+
