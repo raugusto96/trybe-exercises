0128a898287001a685a45e6761e978cc38635038
Rodrigo Augusto <rodrigoaugusto96@outlook.com>
2021-06-09 19:36:46 -0300

    Finish exercise 1

---
+++ Fundamentos do Desenvolvimento Web/bloco_6/html-e-css-forms-flexbox-e-responsivo_1/exercises/script.js
@@ -0,0 +1,37 @@
+const brazilStates = {
+  AC: "Acre",
+  AL: "Alagoas",
+  AP: "Amapá",
+  AM: "Amazonas",
+  BA: "Bahia",
+  CE: "Ceará",
+  ES: "Espírito Santo",
+  DF: "Distrito Federal",
+  GO: "Goiás",
+  MA: "Maranhão",
+  MT: "Mato Grosso",
+  MS: "Mato Grosso do Sul",
+  MG: "Minas Gerais",
+  PA: "Pará",
+  PB: "Paraíba",
+  PR: "Paraná",
+  PE: "Pernambuco",
+  PI: "Piauí",
+  RJ: "Rio de Janeiro",
+  RN: "Rio Grande do Norte",
+  RS: "Rio Grande do Sul",
+  RO: "Rondônia",
+  RR: "Roraima",
+  SC: "Santa Catarina",
+  SP: "São Paulo",
+  SE: "Sergipe",
+  TO: "Tocantins",
+};
+
+for (let key in brazilStates) {
+  const statesDropdown = document.getElementById('states');
+  const stateOption = document.createElement('option');
+  stateOption.value = key;
+  stateOption.innerHTML = brazilStates[key];
+  statesDropdown.appendChild(stateOption);
+}
