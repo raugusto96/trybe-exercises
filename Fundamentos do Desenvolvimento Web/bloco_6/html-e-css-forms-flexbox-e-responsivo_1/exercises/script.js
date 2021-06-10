7fe8bb0c6fa848eca8ffdaf07143142db8a00ffa
Rodrigo Augusto <rodrigoaugusto96@outlook.com>
2021-06-10 09:41:26 -0300

    Finish exercises

--- Fundamentos do Desenvolvimento Web/bloco_6/html-e-css-forms-flexbox-e-responsivo_1/exercises/script.js
+++ Fundamentos do Desenvolvimento Web/bloco_6/html-e-css-forms-flexbox-e-responsivo_1/exercises/script.js
@@ -36,27 +36,83 @@ for (let key in brazilStates) {
   statesDropdown.appendChild(stateOption);
 }
 
-// const inputDate = document.getElementById('date');
+function checkEmail() {
+  const email = document.querySelector('#email');
+  let emailInput = email.value;
+  const formatEmail = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+$/.test(emailInput);
+  if (!formatEmail && emailInput.length) {
+    email.value = '';
+    alert('Email inválido');
+    return false
+  }
+  return formatEmail;
+}
 
-// inputDate.addEventListener('input', () => {
-//   const startDate = document.getElementById('date').value;
-//   let day = Number(startDate[0] + startDate[1]); 
-//   let month = Number(startDate[3] + startDate[4]);
-//   let year = Number(startDate[6] + startDate[7] + startDate[8] + startDate[9]);
+const name = document.getElementById('full-name');
+const email = document.getElementById('email');
+const cpf = document.getElementById('cpf');
+const address = document.getElementById('address');
+const city = document.getElementById('city');
+const states = document.getElementById('states');
+const resumeCurriculum = document.getElementById('resume');
+const office = document.getElementById('office');
+const descriptionOffice = document.getElementById('resume-office');
+const initialDate = document.getElementById('date');
+
+const formsElements = [name, email, cpf, address, city, states, resumeCurriculum, office, descriptionOffice, initialDate];
+const results = document.querySelector('.curriculum-content');
+const submitButton = document.getElementById('submit-button');
+const resetButton = document.getElementById('reset-button');
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  for (let key in formsElements) {
+    if (checkEmail() && verifyDate()) {
+      const result = document.createElement('div');
+      result.innerHTML = formsElements[key].value;
+      results.appendChild(result);
+    }
+  }
+});
+
+resetButton.addEventListener('click', () => {
+  document.getElementById('my-form').reset();
+  const curriculum = document.querySelectorAll('.curriculum-content');
+
+  for (let index = 0; index < curriculum.length; index += 1) {
+    curriculum[index].innerHTML = '';
+  }
+});
+
+function verifyDate() {
+  const inputDate = document.getElementById('date');
+  if (inputDate.value.length === 0) {
+    return alert('A data não foi preenchida!');
+  }
+
+  let regex = /^\d\d\/\d\d\/\d\d\d\d$/; // regex do gabarito;
+  
+  if (!regex.test(inputDate.value)) {
+    return alert('Data: Formato inválido!');
+  }
+
+  let splitted = inputDate.value.split('/');
+  let day = splitted[0]; 
+  let month = splitted[1];
+  let year = splitted[2];
     
-//   if (day > 0) {
-//     console.log('Funciona');
-//   } 
-//   else {
-//     alert('O dia é inválido');
-//   }
-  // if (month > 0 && month <= 12) {
-  //   alert('O mês é inválido');
-  // }
-  // if (year > 0) {
-  //   alert('O ano é inválido');
-  // }
-// });
+  if (day < 0 && day > 31) {
+    return alert('Dia inválido!');
+  }
+  if (month < 0 && month > 12) {
+    return alert('Mês inválido!');
+  }
+  if (year < 0) {
+    return alert('Ano inválido!');
+  }
+  return true;
+}
 
 
 
