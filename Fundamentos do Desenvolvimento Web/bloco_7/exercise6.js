1c37e514c462a1da50136a9e222b04c2dcac4711
Rodrigo Augusto <rodrigoaugusto96@outlook.com>
2021-06-17 18:30:24 -0300

    Exercise 6

---
+++ Fundamentos do Desenvolvimento Web/bloco_7/exercise6.js
@@ -0,0 +1,7 @@
+const findX = (string) => {
+  const stringParameter = 'Tryber x aqui';
+  const newString = stringParameter.replace('x', string);
+  return newString;
+}
+
+console.log(findX('Leticia'));
