e53f0645be6a0ea8c23eb847590b54f6f1761bc6
Rodrigo Augusto <rodrigoaugusto96@outlook.com>
2021-06-18 15:10:01 -0300

    Content exercise

---
+++ Fundamentos do Desenvolvimento Web/bloco_7/introducao-a-javascript-es6-e-testes-unitarios_2/object-keys.js
@@ -0,0 +1,27 @@
+const student1 = {
+  Html: 'Muito Bom',
+  Css: 'Bom',
+  JavaScript: 'Ótimo',
+  SoftSkills: 'Ótimo',
+};
+
+const student2 = {
+  Html: 'Bom',
+  Css: 'Ótimo',
+  JavaScript: 'Ruim',
+  SoftSkills: 'Ótimo',
+  Git: 'Bom', // chave adicionada
+};
+
+const listSkills = (student) => {
+  const arraySkills = Object.keys(student);
+  for (index in arraySkills) {
+    console.log(`${arraySkills[index]}, Nível: ${student[arraySkills[index]]}`);
+  }
+};
+
+console.log('Estudante 1');
+listSkills(student1);
+
+console.log('Estudante 2');
+listSkills(student2);
