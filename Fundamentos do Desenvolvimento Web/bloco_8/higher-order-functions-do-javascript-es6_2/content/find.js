16e79c8f5fdbe9c6049e8ad0ddd585f6e50f129b
Rodrigo <rodris@pop-os.localdomain>
2021-06-24 15:46:20 -0300

    Fixation exercises

---
+++ Fundamentos do Desenvolvimento Web/bloco_8/higher-order-functions-do-javascript-es6_2/content/find.js
@@ -0,0 +1,16 @@
+const numbers = [19, 21, 30, 3, 45, 22, 15];
+
+// Adiciona seu código aqui
+const findDivisibleBy3And5 = (array) => numbers.find((number) => number % 3 === 0 && number % 5 === 0);
+
+console.log(findDivisibleBy3And5(numbers))
+
+// // Exercise - 2
+
+const names = ['João', 'Irene', 'Fernando', 'Maria'];
+
+// Adicione seu código aqui:
+const findNameWithFiveLetters = () => {
+}
+
+console.log(findNameWithFiveLetters());
