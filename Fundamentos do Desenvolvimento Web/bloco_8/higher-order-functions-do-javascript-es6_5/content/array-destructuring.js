8395338629a3aaf9907d2cc13031a9e335c39a55
Rodrigo Augusto <rodrigoaugusto96@outlook.com>
2021-06-29 15:53:26 -0300

    Fix exercise 2

---
+++ Fundamentos do Desenvolvimento Web/bloco_8/higher-order-functions-do-javascript-es6_5/content/array-destructuring.js
@@ -0,0 +1,35 @@
+const saudacoes = ['Olá', (saudacao) => console.log(saudacao)];
+
+saudacoes[1](saudacoes[0]); // Olá
+
+// Produza o mesmo resultado acima, porém utilizando array destructuring
+
+const  [firstGreeting, secondGreeting] = saudacoes;
+
+secondGreeting(firstGreeting);
+
+// // // // // // // // // //
+
+let comida = 'gato';
+let animal = 'água';
+let bebida = 'arroz';
+
+console.log(comida, animal, bebida); // arroz gato água
+
+// // Utilizando array destructuring, faça com que os valores apareçam nas variáveis correspondentes ao seu verdadeiro tipo
+
+[comida = 'arroz', animal = 'gato', bebida = 'água'] = [];
+
+console.log(comida, animal, bebida);
+
+// // // // // // // //
+
+let numerosPares = [1, 3, 5, 6, 8, 10, 12];
+
+console.log(numerosPares); // [6, 8, 10, 12];
+
+// Utilize array destructuring para produzir o resultado esperado pelo console.log abaixo
+
+const [,,,six, eight, ten, twelve] = numerosPares;
+
+console.log(six, eight, ten, twelve); // [6, 8, 10, 12];
