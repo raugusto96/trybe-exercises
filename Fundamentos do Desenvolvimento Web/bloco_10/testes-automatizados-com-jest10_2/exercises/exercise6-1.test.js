6772923773c4066328bba76b4e72c51a27193507
Rodrigo Augusto <rodrigoaugusto96@outlook.com>
2021-07-14 19:17:49 -0300

    Exercise 6-1

---
+++ Fundamentos do Desenvolvimento Web/bloco_10/testes-automatizados-com-jest10_2/exercises/exercise6-1.test.js
@@ -0,0 +1,46 @@
+const Animals = [
+  { name: 'Dorminhoco', age: 1, type: 'Dog' },
+  { name: 'Soneca', age: 2, type: 'Dog' },
+  { name: 'Preguiça', age: 5, type: 'Cat' },
+];
+
+// Adicione o código aqui.
+const findAnimalByName = (name) => (
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const animal = Animals.find((animal) => animal.name === name);
+      if (animal) {
+        return resolve(animal);
+      };
+      const messageError = 'Nenhum animal com esse nome!';
+      return reject(messageError);
+    }, 100);
+  })
+);
+
+// Adicione o código aqui.
+const getAnimal = (name) => {
+  return findAnimalByName(name)
+    .then(animal => animal);
+};
+// ---------------------
+
+describe('Testando promise - findAnimalByName', () => {
+  describe('Quando existe o animal com o nome procurado', () => {
+    test('Retorne o objeto do animal', () => {
+      expect.assertions(1);
+      return getAnimal('Dorminhoco').then(animal => {
+        expect(animal).toEqual({ name: 'Dorminhoco', age: 1, type: 'Dog' });
+      });
+    });
+  });
+
+  describe('Quando não existe o animal com o nome procurado', () => {
+    test('Retorna um erro', () => {
+      expect.assertions(1);
+      return getAnimal('Bob').catch(error =>
+        expect(error).toEqual('Nenhum animal com esse nome!')
+      );
+    });
+  });
+});
