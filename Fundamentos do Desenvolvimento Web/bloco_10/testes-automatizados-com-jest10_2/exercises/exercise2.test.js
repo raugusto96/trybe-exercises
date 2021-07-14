8337ab954289cb8297b25dddbdce2176de498b89
Rodrigo Augusto <rodrigoaugusto96@outlook.com>
2021-07-14 15:56:13 -0300

    Exercise 2

---
+++ Fundamentos do Desenvolvimento Web/bloco_10/testes-automatizados-com-jest10_2/exercises/exercise2.test.js
@@ -0,0 +1,21 @@
+const {
+  getUserName,
+} = require('./exercise2');
+
+describe('getUserName - promise', () => {
+  describe('quando o user id exists', () => {
+    it('retorna o nome do usuário', () => {
+      expect.assertions(1);
+      return getUserName(4).then((data) => expect(data).toEqual('Mark'));
+    });
+  });
+
+  describe('quando o user id dont exist', () => {
+    it('retorna um erro', () => {
+      expect.assertions(1);
+      return getUserName(8).catch(error => 
+        expect(error).toEqual({ error: 'User with 8 not found.' })
+        );
+    });
+  });
+});
