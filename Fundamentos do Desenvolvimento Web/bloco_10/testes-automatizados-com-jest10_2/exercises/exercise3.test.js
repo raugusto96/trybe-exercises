14fb21909ff61c0bffa0acbc92355922c51e1629
Rodrigo Augusto <rodrigoaugusto96@outlook.com>
2021-07-14 16:17:26 -0300

    Exercise 3

--- Fundamentos do Desenvolvimento Web/bloco_10/testes-automatizados-com-jest10_2/exercises/exercise3.test.js
+++ Fundamentos do Desenvolvimento Web/bloco_10/testes-automatizados-com-jest10_2/exercises/exercise3.test.js
@@ -2,7 +2,30 @@ const {
   getUserName,
 } = require('./exercise2');
 
-describe('getUserName - promise', () => {
+// Com try/catch
+describe('getUserName - async/await', () => {
+  describe('quando o user id exists', () => {
+    it('retorna o nome do usuário', async () => {
+      expect.assertions(1);
+      const data = await getUserName(4);
+      expect(data).toEqual('Mark');
+    });
+  });
+
+  describe('quando o user id dont exist', () => {
+    it('retorna um erro', async () => {
+      expect.assertions(1);
+      try {
+        await getUserName(8);
+      } catch (error) {
+        expect(error).toEqual({ error: 'User with 8 not found.' });
+    }
+  });
+});
+});
+
+// Sem try/catch
+describe('getUserName - resolves/rejects', () => {
   describe('quando o user id exists', () => {
     it('retorna o nome do usuário', async () => {
       expect.assertions(1);
